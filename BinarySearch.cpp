88c45d62a681c36b5c7d672921664d98a06fd764
Razfar14 <rajarangkuty14@gmail.com>
2025-05-07 13:28:34 +0700

    4 commit

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -17,6 +17,12 @@ void input (){
             cout << "\n[!] Jumlah elemen tidak lebih dari 10. Silakan coba lagi.\n";
         }
     }
-    
-    
+
+    cout << "\n======================= \n";
+    cout << "\nMasukkan Elemen Array : \n";
+    cout << "\n======================= \n";
+    for(int i = 0; i < nPanjang; i++){
+        cout << "Data ke-"<< (i+1) << "=";
+        cin >> element[i];
+    }
 }
